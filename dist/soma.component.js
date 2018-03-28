f31a11e3a0273e2e15ec37117a23776354776b89
Gislainy Crisostomo Velasco <gislainycrisostomo@gmail.com>
2018-03-28 13:19:55 -0300

    iniciando

---
+++ dist/soma.component.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var somalib_1 = require("@hoda5/somalib");
+console.log('1+2 =', somalib_1.soma(1, 2));
+//# sourceMappingURL=soma.component.js.map
---
+++ src/soma.component.ts
@@ -0,0 +1,3 @@
+import { soma } from '@hoda5/somalib';
+
+console.log('1+2 =', soma(1, 2))
