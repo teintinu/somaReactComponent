d1734d8950bda3417d912565cca8eadded35baaf
Gislainy Crisostomo Velasco <gislainycrisostomo@gmail.com>
2018-03-28 14:09:43 -0300

    app example react

---
+++ dist/components/Hello.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="react" />
+import * as React from "react";
+export interface HelloProps {
+    compiler: string;
+    framework: string;
+}
+export declare class Hello extends React.Component<HelloProps, {}> {
+    render(): JSX.Element;
+}
