5c37444f728167b08a11b461844b2f6d64894a56
Ramon Henrique <ramon_henrique34@hotmail.com>
2018-04-03 13:16:16 -0300

    soma erro

--- src/index.js
+++ src/index.js
@@ -1,7 +1,30 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 Object.defineProperty(exports, "__esModule", { value: true });
 var React = require("react");
-var react_dom_1 = require("react-dom");
-var App_1 = require("./components/App");
-react_dom_1.render(React.createElement(App_1.default, { message: "rapaziadaaaa" }), document.getElementById('root'));
+var ReactDom = require("react-dom");
+var input_1 = require("./components/input");
+var Calcula = /** @class */ (function (_super) {
+    __extends(Calcula, _super);
+    function Calcula(props) {
+        return _super.call(this, props) || this;
+    }
+    Calcula.prototype.render = function () {
+        return (React.createElement("div", null,
+            React.createElement(input_1.Input, { name: "inputA", type: "number", placeholder: "Digite o valor de A" }),
+            React.createElement(input_1.Input, { name: "inputB", type: "number", placeholder: "Digite o valor de B" })));
+    };
+    return Calcula;
+}(React.Component));
+var root = document.getElementById('root');
+ReactDom.render(React.createElement(Calcula, null), root);
 //# sourceMappingURL=index.js.map
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,59 @@
 import * as React from 'react';
-import { render } from 'react-dom';
-import App from './components/App';
+import * as ReactDom from 'react-dom';
 
-render(
-    <App message="rapaziadaaaa" />,
-    document.getElementById('root'),
-);
+import { soma } from "@hoda5/somalib";
+class Calcula extends React.Component<any, any> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            a: 0,
+            b: 0,
+            r: 0
+        };
+        this.calcular = this.calcular.bind(this);
+        this.valorA = this.valorA.bind(this);
+        this.valorB = this.valorB.bind(this);
+    }
+    calcular() {
+        const self = this
+        const res = soma(self.state.a, self.state.b)
+        this.setState({
+            r: self.state.a+self.state.b
+        })
+    }
+    valorA(event: any) {
+        this.setState({
+            a: parseInt(event.target.value)
+        })
+    }
+    valorB(event: any) {
+        this.setState({
+            b: parseInt(event.target.value)
+        })
+    }
+    render() {
+        return (
+            <div>
+                <input
+                    name="inputA"
+                    value = {this.state.a}
+                    type="number"
+                    onChange={this.valorA}
+                    placeholder="Digite o valor de A"
+                    />
+                <input
+                    name="inputB"
+                    value = {this.state.b}
+                    type="number"
+                    onChange={this.valorB}
+                    placeholder="Digite o valor de B"
+                />
+                <button onClick={this.calcular}>Calcular</button>
+                {this.state.r}
+            </div>
+        );
+    }
+}
+const root = document.getElementById('root') as HTMLElement;
+
+ReactDom.render(<Calcula />, root);
