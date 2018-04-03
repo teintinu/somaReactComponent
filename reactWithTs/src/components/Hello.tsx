6560c572d184ff5667be7f6303d5ef7b1b9cd12e
MarcelloVSilva <marcellovcs@gmail.com>
2018-04-03 01:37:29 -0300

    soma

--- reactWithTs/src/components/Hello.tsx
+++ reactWithTs/src/components/Hello.tsx
@@ -1,9 +1,20 @@
 import * as React from "react";
 
-export interface HelloProps { 
-    compiler: string; 
-    framework: string; 
+export interface Soma { 
+    a: number; 
+    b: number;
+    resultado: number; 
 }
 
+const onChangeInput = () => 
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(e.target.name, e.target.value);
+  };
 
-export const Hello = (props: HelloProps) => <h1>Hello from {props.compiler} and {props.framework}!</h1>;
+export const Soma = 
+    (props: Soma) => 
+    <div>
+        a<input type="number" id="a" onChange={onChangeInput()}> </input>
+        b<input type="number" id="b" onChange={onChangeInput()}> </input>
+        <a type="number">{props.resultado}</a>
+    </div>;
--- reactWithTs/src/index.tsx
+++ reactWithTs/src/index.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import { Hello } from "./components/Hello";
+import { Soma } from "./components/Hello";
 
 ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
+    <Soma />,
     document.getElementById("example")
 );
