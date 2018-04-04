007ae58acdc9a20c64a49cbb0bef8fff7e57359e
MarcelloVSilva <marcellovcs@gmail.com>
2018-04-04 09:21:19 -0300

    testes

--- reactWithTs/src/components/Hello.tsx
+++ reactWithTs/src/components/Hello.tsx
@@ -7,34 +7,28 @@ type stateType = {
     b: Number;
     resultado: Number;
 }
-export class Soma extends Component {
+export class Soma extends React.Component<{}, stateType> {
     constructor(props: stateType) {
       super(props);
         this.state = {
-            a: 0, 
-            b: 0, 
-            resultado: 0,
+            a: null, 
+            b: null, 
+            resultado: null,
         };
         this.render();
     }
 
     public onChangeInput = () => 
         (e: React.ChangeEvent<HTMLInputElement>) => {
-            let res = soma(1, 3)
-            alert(res)
+            let resultado = soma(1, 3)
+            this.setState({resultado}); 
     };
-
-    public state: {
-        a: Number; 
-        b: Number;
-        resultado: Number; 
-    }
 
     public render() {
         return (
             <div>
-                a<input  id="a" onChange={this.onChangeInput()}>{this.state.a}</input>
-                b<input  id="b" onChange={this.onChangeInput()}>{this.state.b}</input>
+                a<input  id="a" onChange={this.onChangeInput()}/>
+                b<input  id="b" onChange={this.onChangeInput()}/>
                 resultado<a>{this.state.resultado}</a>
             </div>
         );
