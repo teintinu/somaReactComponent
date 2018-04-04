2a82b0a1293145c7a96e5c97c43703d0dae9ac81
MarcelloVSilva <marcellovcs@gmail.com>
2018-04-04 08:09:58 -0300

    soma

--- reactWithTs/src/components/Hello.tsx
+++ reactWithTs/src/components/Hello.tsx
@@ -1,20 +1,42 @@
 import * as React from "react";
+import { Component } from "react";
+import { soma } from "@hoda5/somalib";
 
-export interface Soma { 
-    a: number; 
-    b: number;
-    resultado: number; 
+type stateType = {
+    a: Number; 
+    b: Number;
+    resultado: Number;
 }
+export class Soma extends Component {
+    constructor(props: stateType) {
+      super(props);
+        this.state = {
+            a: 0, 
+            b: 0, 
+            resultado: 0,
+        };
+        this.render();
+    }
 
-const onChangeInput = () => 
-    (e: React.ChangeEvent<HTMLInputElement>) => {
-        console.log(e.target.name, e.target.value);
-  };
+    public onChangeInput = () => 
+        (e: React.ChangeEvent<HTMLInputElement>) => {
+            let res = soma(1, 3)
+            alert(res)
+    };
 
-export const Soma = 
-    (props: Soma) => 
-    <div>
-        a<input type="number" id="a" onChange={onChangeInput()}> </input>
-        b<input type="number" id="b" onChange={onChangeInput()}> </input>
-        <a type="number">{props.resultado}</a>
-    </div>;
+    public state: {
+        a: Number; 
+        b: Number;
+        resultado: Number; 
+    }
+
+    public render() {
+        return (
+            <div>
+                a<input  id="a" onChange={this.onChangeInput()}>{this.state.a}</input>
+                b<input  id="b" onChange={this.onChangeInput()}>{this.state.b}</input>
+                resultado<a>{this.state.resultado}</a>
+            </div>
+        );
+    }
+}
